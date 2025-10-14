0cca053b592e9975a820610ec011fe1ba79c83dc
Ulisses <Ulisses.wrk@gmail.com>
2025-10-14 19:56:20 -0300

    add: adiciona ex2 ao repositorio

--- src/components/Ex2.jsx
+++ src/components/Ex2.jsx
@@ -6,7 +6,11 @@ export default function Ex2() {
   return (
     <div className="my-8">
         <h1 className="text-2xl font-bold mb-4">Ex2 - Flexbox Básico</h1>
-      {/* Resolva aqui */}
+      <div className="flex justify-center gap-5 m-3 ">
+          <div className="bg-blue-400 h-10 w-10"></div>
+          <div className="bg-red-400 h-10 w-10"></div>
+          <div className="bg-green-400 h-10 w-10"></div>
+      </div>
     </div>
   );
 }
