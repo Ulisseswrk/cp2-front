aeb9e94ca97c5f8f8e9f6b70ee682c96caeecb2f
Ulisses <Ulisses.wrk@gmail.com>
2025-10-14 20:52:18 -0300

    add: adiciona o ex6 ao repositorio

--- src/components/Ex6.jsx
+++ src/components/Ex6.jsx
@@ -11,8 +11,12 @@
 export default function Ex6() {
   return (
     <div className="my-8">
-        <h1 className="text-2xl font-bold mb-4">Ex6 - Layout com Grid</h1>
-      {/* Resolva aqui */}
+        <h1 className="grid text-2xl font-bold mb-4">Ex6 - Layout com Grid</h1>
+      <div className="h-[20rem]">
+        <header className="w-[100%] h-[80px] bg-red-300"></header>
+        <main className="bg-red-400 h-[140px]"></main>
+        <footer className=" bg-red-500 h-[80px] w-[100%]"></footer>
+      </div>
     </div>
   );
 }
